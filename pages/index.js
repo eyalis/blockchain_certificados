9ef853f31176746e2c6a1ae39ee2b82cc5609cd3
Eyalis <eszewkis@yahoo.com>
2023-05-01 00:28:42 -0400

    Se separaron los módulos para hacerlos más ordenados y se comentó el código

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-// pages/index.js
-
+// Import required dependencies
 import React, { useState } from "react";
 import {
     Container,
@@ -15,8 +14,10 @@ import { AccountBalanceWallet } from "@mui/icons-material";
 import QRCode from "qrcode.react";
 import Link from "@mui/material/Link";
 
+// Import utility functions for interacting with the smart contract
 import { connectWallet, emitirCertificado, verificarCertificado } from "./web3Utils";
 
+// Apply custom styles to the container
 const StyledContainer = styled(Container)(({ theme }) => ({
     display: "flex",
     flexDirection: "column",
@@ -29,109 +30,9 @@ const StyledContainer = styled(Container)(({ theme }) => ({
     },
 }));
 
-const CONTRACT_ABI = [
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "_id",
-                "type": "uint256"
-            },
-            {
-                "internalType": "string",
-                "name": "_nombre",
-                "type": "string"
-            },
-            {
-                "internalType": "string",
-                "name": "_carrera",
-                "type": "string"
-            }
-        ],
-        "name": "emitirCertificado",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "certificados",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "id",
-                "type": "uint256"
-            },
-            {
-                "internalType": "string",
-                "name": "nombre",
-                "type": "string"
-            },
-            {
-                "internalType": "string",
-                "name": "carrera",
-                "type": "string"
-            },
-            {
-                "internalType": "bool",
-                "name": "emitido",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "contadorCertificados",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "_id",
-                "type": "uint256"
-            }
-        ],
-        "name": "verificarCertificado",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            },
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            },
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
-];
-
+// Main App component
 const App = () => {
+    // Declare state variables
     const [web3, setWeb3] = useState(null);
     const [account, setAccount] = useState(null);
     const [contract, setContract] = useState(null);
@@ -141,6 +42,7 @@ const App = () => {
     const [carrera, setCarrera] = useState("");
     const [mensaje, setMensaje] = useState("");
 
+    // Function to handle wallet connection
     const handleConnectWallet = async () => {
         try {
             const { web3Instance, account, contractInstance } = await connectWallet();
@@ -152,56 +54,60 @@ const App = () => {
         }
     };
 
+    // Function to handle certificate issuance
     const handleEmitirCertificado = async () => {
         if (contract) {
             try {
+                const gasLimit = 21000; // Set the desired gas limit for the transaction
 
-                const gasLimit = 21000; // Ajusta el límite de gas a tu valor deseado
-
+                // Call the smart contract function to issue a certificate
                 await contract.methods
                     .emitirCertificado(id, nombre, carrera)
                     .send({ from: account });
 
-                setMensaje("Certificado emitido con éxito");
+                setMensaje("Certificate issued successfully");
             } catch (error) {
-                setMensaje("Error al emitir el certificado");
+                setMensaje("Error issuing the certificate");
             }
         } else {
-            setMensaje("Por favor, conecte su billetera");
+            setMensaje("Please, connect your wallet");
         }
     };
 
+    // Function to handle certificate verification
     const handleVerificarCertificado = async () => {
         if (contract) {
             try {
+                // Call the smart contract function to verify a certificate
                 const certInfo = await contract.methods.verificarCertificado(id).call();
                 setNombre(certInfo[0]);
                 setCarrera(certInfo[1]);
-                setMensaje(certInfo[2] ? "Certificado válido" : "Certificado no válido");
+                setMensaje(certInfo[2] ? "Valid certificate" : "Invalid certificate");
             } catch (error) {
-                setMensaje("Error al verificar el certificado");
+                setMensaje("Error verifying the certificate");
             }
         } else {
-            setMensaje("Por favor, conecte su billetera");
+            setMensaje("Please, connect your wallet");
         }
     };
 
+    // Render the main component
     return (
         <StyledContainer maxWidth="sm">
-
+            {/* Display the title */}
             <Box mt={4}>
                 <Typography variant="h4" align="center">
-                    Certificados Universitarios
+                    University Certificates
                 </Typography>
             </Box>
 
+            {/* Display the QR code */}
             <Box mt={4}>
                 <Typography
                     variant="h6"
                     align="center"
                     sx={{ fontSize: "1rem", paddingBottom: "1rem" }}
                 >
-
                 </Typography>
                 <Box display="flex" justifyContent="center" alignItems="center" mb={4}>
                     <Link href="https://testnet.bscscan.com/address/0x5fed74b8d1ff747f937469e3a712bdb5d046d37f">
@@ -212,7 +118,7 @@ const App = () => {
                 </Box>
             </Box>
 
-
+            {/* Display "Connect Wallet" button if wallet is not connected */}
             {!web3 && (
                 <Button
                     variant="outlined"
@@ -220,43 +126,49 @@ const App = () => {
                     startIcon={<AccountBalanceWallet />}
                     onClick={handleConnectWallet}
                 >
-                    Conectar billetera
+                    Connect wallet
                 </Button>
             )}
 
+            {/* Display form and actions if wallet is connected */}
             {web3 && (
                 <>
                     <Typography variant="subtitle1" align="center" mt={2}>
-                        Cuenta conectada: {account}
+                        Connected account: {account}
                     </Typography>
                     <Box mt={4}>
                         <form>
+                            {/* Certificate ID input */}
                             <TextField
                                 type="number"
                                 label="ID"
                                 value={id}
                                 onChange={(e) => setId(e.target.value)}
                             />
+                            {/* Student's name input */}
                             <TextField
-                                label="Nombre"
+                                label="Name"
                                 value={nombre}
                                 onChange={(e) => setNombre(e.target.value)}
                             />
+                            {/* Degree input */}
                             <TextField
-                                label="Carrera"
+                                label="Degree"
                                 value={carrera}
                                 onChange={(e) => setCarrera(e.target.value)}
                             />
+                            {/* Action buttons */}
                             <Stack spacing={2} direction="row">
                                 <Button variant="contained" color="primary" onClick={handleEmitirCertificado}>
-                                    Emitir certificado
+                                    Issue certificate
                                 </Button>
                                 <Button variant="contained" color="secondary" onClick={handleVerificarCertificado}>
-                                    Verificar certificado
+                                    Verify certificate
                                 </Button>
                             </Stack>
                         </form>
                     </Box>
+                    {/* Display message, if any */}
                     {mensaje && (
                         <Alert severity="info" sx={{ mb: 2 }}>
                             {mensaje}
--- pages/web3Utils.js
+++ pages/web3Utils.js
@@ -1,26 +1,43 @@
+// Import Web3 library for interacting with Ethereum blockchain
 import Web3 from "web3";
+// Import the contract ABI (Application Binary Interface) from a JSON file
 import CONTRACT_ABI from "./contractAbi.json";
 
+// Define the contract address for the smart contract
 const CONTRACT_ADDRESS = "0x5feD74B8d1fF747f937469e3a712bdB5D046D37F";
 
+// Function to connect the user's wallet
 export const connectWallet = async () => {
+    // Check if window.ethereum (MetaMask) is available
     if (window.ethereum) {
+        // Create a new Web3 instance with the injected provider
         const web3Instance = new Web3(window.ethereum);
+        // Request user's account access
         const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+        // Get the first account in the user's wallet
         const account = accounts[0];
+        // Create a contract instance with the ABI and contract address
         const contractInstance = new web3Instance.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+        // Return the created Web3 instance, user's account and contract instance
         return { web3Instance, account, contractInstance };
     } else {
-        throw new Error("Por favor, instale MetaMask para usar esta aplicación");
+        // Throw an error if MetaMask is not installed
+        throw new Error("Please install MetaMask to use this application");
     }
 };
 
+// Function to emit a certificate using the smart contract
 export const emitirCertificado = async (contract, id, nombre, carrera, account) => {
+    // Set the gas limit for the transaction
     const gasLimit = 21000;
+    // Call the emitirCertificado function from the smart contract and send the transaction
     await contract.methods.emitirCertificado(id, nombre, carrera).send({ from: account });
 };
 
+// Function to verify a certificate using the smart contract
 export const verificarCertificado = async (contract, id) => {
+    // Call the verificarCertificado function from the smart contract and get the certificate information
     const certInfo = await contract.methods.verificarCertificado(id).call();
+    // Return the fetched certificate information
     return certInfo;
 };
